add6d295c7e5d75c3cb33feebf66ab83c317a643
MicrocontrollersDev <66657148+MicrocontrollersDev@users.noreply.github.com>
2022-03-17 18:12:30 -0700

    fix guardian beam rendering (#1, MC-165595)
    fixes https://bugs.mojang.com/browse/MC-165595

---
+++ src/main/java/cc/woverflow/debugify/mixins/mc165595/GuardianEntityRendererMixin.java
@@ -0,0 +1,20 @@
+package cc.woverflow.debugify.mixins.mc165595;
+
+import net.minecraft.client.render.entity.GuardianEntityRenderer;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * Adapted from a Sodium PR under GPL 3 License
+ * https://github.com/AMereBagatelle/sodium-fabric/blob/guardian-beam-fix/LICENSE.txt
+ * @author AMereBagatelle
+ */
+@Mixin(GuardianEntityRenderer.class)
+public class GuardianEntityRendererMixin {
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/world/World.getTime()J"))
+    private long useCorrectTime(World world) {
+        return world.getTimeOfDay();
+    }
+}
