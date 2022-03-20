94967e16982677195f40128012a4a29f8227d86f
isXander <xandersmith2008@gmail.com>
2022-03-20 11:38:34 +0000

    add configuration for metrics

--- common/src/main/java/cc/woverflow/debugify/Debugify.java
+++ common/src/main/java/cc/woverflow/debugify/Debugify.java
@@ -1,14 +1,23 @@
 package cc.woverflow.debugify;
 
 import cc.woverflow.debugify.api.metrics.UniqueUsersMetric;
+import cc.woverflow.debugify.config.DebugifyConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Debugify {
     public static Logger logger = LoggerFactory.getLogger("Debugify");
 
+    public static DebugifyConfig config;
+
     public static void onInitialize() {
-        new Thread(UniqueUsersMetric::putApi, "debugify-concurrent").start();
+        config = new DebugifyConfig();
+        config.load();
+
+        if (config.doMetrics) {
+            new Thread(UniqueUsersMetric::putApi, "debugify-concurrent").start();
+        }
+
         logger.info("Successfully Debugify'd your game!");
     }
 }
