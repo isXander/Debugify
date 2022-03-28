053d28563babc249a1fb79b0cdf675e7a26ba768
MicrocontrollersDev <66657148+MicrocontrollersDev@users.noreply.github.com>
2022-03-28 08:19:44 -0700

    mc-111516 (#16)

--- common/src/main/java/cc/woverflow/debugify/mixins/client/mc121772/MouseMixin.java
+++ common/src/main/java/cc/woverflow/debugify/mixins/client/mc121772/MouseMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(Mouse.class)
 public class MouseMixin {
     /**
-     * adapted from https://github.com/nelson2tm/shift-scroll-fix
+     * Adapted from https://github.com/nelson2tm/shift-scroll-fix
      * under MIT license
      */
     @ModifyVariable(method = "onMouseScroll", at = @At("HEAD"), ordinal = 1, argsOnly = true)
