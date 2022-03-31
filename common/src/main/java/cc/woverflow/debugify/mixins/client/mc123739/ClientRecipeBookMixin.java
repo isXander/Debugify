3fb25aa337a10b3edf0a131a0e7e016fa03d76f8
aws404 <aws40404@gmail.com>
2022-03-31 18:04:54 +1100

    New, more stable implementation

--- common/src/main/java/cc/woverflow/debugify/mixins/client/mc123739/ClientRecipeBookMixin.java
+++ common/src/main/java/cc/woverflow/debugify/mixins/client/mc123739/ClientRecipeBookMixin.java
@@ -2,36 +2,36 @@ package cc.woverflow.debugify.mixins.client.mc123739;
 
 import net.minecraft.client.gui.screen.recipebook.RecipeResultCollection;
 import net.minecraft.client.recipebook.ClientRecipeBook;
+import net.minecraft.client.recipebook.RecipeBookGroup;
 import net.minecraft.recipe.Recipe;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 
 @Mixin(ClientRecipeBook.class)
 public abstract class ClientRecipeBookMixin {
 
-    /**
-     * Method: reload
-     * Anonymous Class: map.forEach((recipeBookGroup, list) -> {
-     *     # HERE #
-     * })
-     */
-    @SuppressWarnings("UnresolvedMixinReference")
-    @Redirect(method = "method_30279", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;map(Ljava/util/function/Function;)Ljava/util/stream/Stream;"))
-    private static Stream<RecipeResultCollection> test(Stream<List<Recipe<?>>> stream, Function<List<Recipe<?>>, RecipeResultCollection> function) {
-        return stream
+    @Redirect(method = "reload", at = @At(value = "INVOKE", target = "Ljava/util/Map;forEach(Ljava/util/function/BiConsumer;)V", ordinal = 0))
+    private void sortRecipes(Map<RecipeBookGroup, List<List<Recipe<?>>>> map, BiConsumer<RecipeBookGroup, List<List<Recipe<?>>>> action) {
+        map.entrySet().stream().map(entry -> Map.entry(entry.getKey(), entry.getValue()
+                .stream()
                 .sorted((o1, o2) -> {
                     if (o1.isEmpty() || o2.isEmpty()) {
                         return 0;
                     }
 
                     return o1.get(0).getId().compareTo(o2.get(0).getId());
-                }).map(function);
+                })
+                .collect(Collectors.toList())
+        )).forEach(entry -> action.accept(entry.getKey(), entry.getValue()));
     }
 
 }
