17840079d862b13df1e5ac3a65f8c00c5f5f0e67
Xander <xandersmith2008@gmail.com>
2022-04-02 20:00:49 +0100

    MC-159163 (closes #36), fix crash on forge

--- common/src/main/java/cc/woverflow/debugify/mixins/server/mc90084/BoatEntityMixin.java
+++ common/src/main/java/cc/woverflow/debugify/mixins/server/mc90084/BoatEntityMixin.java
@@ -3,6 +3,9 @@ package cc.woverflow.debugify.mixins.server.mc90084;
 import net.minecraft.entity.vehicle.BoatEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BoatEntity.class)
 public class BoatEntityMixin {
@@ -10,10 +13,13 @@ public class BoatEntityMixin {
      * This needs to be in server because
      * it's used to update entity position
      *
+     * This should be overwrite but funny architectury
+     * bug hahah FIX IT NOW
+     *
      * @author isXander
      */
-    @Overwrite
-    public double getMountedHeightOffset() {
-        return 0.0;
+    @Inject(method = "getMountedHeightOffset", at = @At("HEAD"), cancellable = true)
+    public void getMountedHeightOffset(CallbackInfoReturnable<Double> cir) {
+        cir.setReturnValue(0.0);
     }
 }
