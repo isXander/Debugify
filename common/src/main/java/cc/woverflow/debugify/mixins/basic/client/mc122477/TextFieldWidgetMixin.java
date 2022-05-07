07b8434b660257f594c0eae7e53a8758357d34ad
isXander <xandersmith2008@gmail.com>
2022-05-07 20:15:10 +0100

    Fix for MC-122477 causing only backspace to work in textfields on linux (closes #77)

--- common/src/main/java/cc/woverflow/debugify/mixins/basic/client/mc122477/TextFieldWidgetMixin.java
+++ common/src/main/java/cc/woverflow/debugify/mixins/basic/client/mc122477/TextFieldWidgetMixin.java
@@ -2,7 +2,10 @@ package cc.woverflow.debugify.mixins.basic.client.mc122477;
 
 import cc.woverflow.debugify.fixes.BugFix;
 import cc.woverflow.debugify.fixes.FixCategory;
+import cc.woverflow.debugify.fixes.mc122477.KeyboardPollCounter;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.text.Text;
 import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -14,18 +17,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @BugFix(id = "MC-122477", category = FixCategory.BASIC, env = BugFix.Env.CLIENT)
 @Mixin(TextFieldWidget.class)
 public class TextFieldWidgetMixin {
-    @Unique
-    private int debugify$ticks = 0;
-
-    @Inject(method = "tick", at = @At("HEAD"))
-    private void onTick(CallbackInfo ci) {
-        if (debugify$ticks < 2)
-            debugify$ticks++;
+    @Inject(method = "<init>(Lnet/minecraft/client/font/TextRenderer;IIIILnet/minecraft/client/gui/widget/TextFieldWidget;Lnet/minecraft/text/Text;)V", at = @At("RETURN"))
+    private void startPolling(TextRenderer textRenderer, int x, int y, int width, int height, TextFieldWidget copyFrom, Text text, CallbackInfo ci) {
+        KeyboardPollCounter.startCounting();
     }
 
     @Inject(method = "charTyped", at = @At("HEAD"), cancellable = true)
     private void onCharTyped(char chr, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        if (debugify$ticks <= 1 && Util.getOperatingSystem() == Util.OperatingSystem.LINUX)
-            cir.setReturnValue(true);
+        if (Util.getOperatingSystem() == Util.OperatingSystem.LINUX) {
+            if (KeyboardPollCounter.getCount() == 1)
+                cir.setReturnValue(true);
+
+            if (KeyboardPollCounter.getCount() >= 1)
+                KeyboardPollCounter.stopCountingAndReset();
+        }
     }
 }
