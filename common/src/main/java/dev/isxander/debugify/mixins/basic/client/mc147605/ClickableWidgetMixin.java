47f3a695dc6bc93eef432c16decf64836f426520
isXander <isxander@users.noreply.github.com>
2022-06-19 13:19:54 +0100

    improve MC-147605 (maybe fix issues)

--- common/src/main/java/dev/isxander/debugify/mixins/basic/client/mc147605/ClickableWidgetMixin.java
+++ common/src/main/java/dev/isxander/debugify/mixins/basic/client/mc147605/ClickableWidgetMixin.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -35,5 +36,20 @@ public abstract class ClickableWidgetMixin {
     @Inject(method = "changeFocus", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ClickableWidget;onFocusedChanged(Z)V", shift = At.Shift.BEFORE))
     protected void changeFocus(boolean lookForwards, CallbackInfoReturnable<Boolean> cir) {
 
+    }
+
+    @ModifyArg(method = "changeFocus", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ClickableWidget;onFocusedChanged(Z)V"))
+    private boolean notifyFocusChange(boolean focusedField) {
+        return isFocused();
+    }
+
+    @ModifyExpressionValue(method = "changeFocus", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/widget/ClickableWidget;focused:Z", opcode = Opcodes.GETFIELD, ordinal = 2))
+    private boolean returnFocusMethod(boolean focusedField) {
+        return isFocused();
+    }
+
+    @ModifyExpressionValue(method = {"getType", "isHovered"}, at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/widget/ClickableWidget;focused:Z", opcode = Opcodes.GETFIELD))
+    private boolean selectionTypeFocusMethod(boolean focusedField) {
+        return isFocused();
     }
 }
