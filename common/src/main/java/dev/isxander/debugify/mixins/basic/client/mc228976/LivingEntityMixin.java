53c202be06d47c1e911935e24d0636258fb75ccc
xander <xander@isxander.dev>
2022-07-21 09:55:47 +0100

    mark MC-228976 as incompatible with lithium

--- common/src/main/java/dev/isxander/debugify/mixins/basic/client/mc228976/LivingEntityMixin.java
+++ common/src/main/java/dev/isxander/debugify/mixins/basic/client/mc228976/LivingEntityMixin.java
@@ -18,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.util.List;
 import java.util.function.Predicate;
 
-@BugFix(id = "MC-228976", category = FixCategory.BASIC, env = BugFix.Env.CLIENT, fabricConflicts = "entitycollisionfpsfix")
+@BugFix(id = "MC-228976", category = FixCategory.BASIC, env = BugFix.Env.CLIENT, fabricConflicts = {"entitycollisionfpsfix", "lithium"})
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
     public LivingEntityMixin(EntityType<?> type, World world) {
