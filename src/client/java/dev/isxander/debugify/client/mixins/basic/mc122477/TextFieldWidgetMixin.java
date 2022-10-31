4ccdd8eab968f34e20cbc1efaaebdff685f699c7
isXander <xandersmith2008@gmail.com>
2022-10-31 17:27:02 +0000

    small fixes fix MC-122477 not being marked as linux-only fix MC-80859 not working bump mixin extras

--- src/client/java/dev/isxander/debugify/client/mixins/basic/mc122477/TextFieldWidgetMixin.java
+++ src/client/java/dev/isxander/debugify/client/mixins/basic/mc122477/TextFieldWidgetMixin.java
@@ -3,6 +3,7 @@ package dev.isxander.debugify.client.mixins.basic.mc122477;
 import dev.isxander.debugify.fixes.BugFix;
 import dev.isxander.debugify.fixes.FixCategory;
 import dev.isxander.debugify.client.fixes.mc122477.KeyboardPollCounter;
+import dev.isxander.debugify.fixes.OS;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.text.Text;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@BugFix(id = "MC-122477", category = FixCategory.BASIC, env = BugFix.Env.CLIENT)
+@BugFix(id = "MC-122477", category = FixCategory.BASIC, env = BugFix.Env.CLIENT, os = OS.LINUX)
 @Mixin(TextFieldWidget.class)
 public class TextFieldWidgetMixin {
     @Inject(method = "<init>(Lnet/minecraft/client/font/TextRenderer;IIIILnet/minecraft/client/gui/widget/TextFieldWidget;Lnet/minecraft/text/Text;)V", at = @At("RETURN"))
