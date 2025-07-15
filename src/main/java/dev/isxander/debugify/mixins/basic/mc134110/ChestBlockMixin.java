e64aaf11cd6e398eb07bec52a0868784437ef36d
MicrocontrollersDev <microcontrollersyt@gmail.com>
2025-07-15 15:48:46 -0700

    fix MC-134110

---
+++ src/main/java/dev/isxander/debugify/mixins/basic/mc134110/ChestBlockMixin.java
@@ -0,0 +1,21 @@
+package dev.isxander.debugify.mixins.basic.mc134110;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import dev.isxander.debugify.fixes.BugFix;
+import dev.isxander.debugify.fixes.FixCategory;
+import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+
+import static net.minecraft.world.level.block.ChestBlock.TYPE;
+
+@BugFix(id = "MC-134110", category = FixCategory.BASIC, env = BugFix.Env.SERVER, description = "Structure mirroring breaking apart double chests")
+@Mixin(ChestBlock.class)
+public class ChestBlockMixin {
+    @WrapMethod(method = "mirror")
+    private BlockState fixChestRotations(BlockState blockState, Mirror mirror, Operation<BlockState> original) {
+        return mirror == Mirror.NONE ? original.call(blockState, mirror) : original.call(blockState, mirror).setValue(TYPE, original.call(blockState, mirror).getValue(TYPE).getOpposite());
+    }
+}
