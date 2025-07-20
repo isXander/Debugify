e8a3c22d9f56a249012ae0fb18145732a8b4baf7
MicrocontrollersDev <microcontrollersyt@gmail.com>
2025-07-20 14:48:12 -0700

    modify shadowed field directly instead of setting it later

--- src/main/java/dev/isxander/debugify/mixins/basic/mc271899/StructureTemplateMixin.java
+++ src/main/java/dev/isxander/debugify/mixins/basic/mc271899/StructureTemplateMixin.java
@@ -9,9 +9,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.Map;
@@ -22,11 +19,5 @@ public class StructureTemplateMixin {
     @Mutable
     @Shadow
     @Final
-    private Map<Block, List<StructureTemplate.StructureBlockInfo>> cache;
-
-    // Use a thread-safe Concurrent Map instead
-    @Inject(method = "<init>", at = @At(value = "TAIL"))
-    private void useConcurrentMap(List list, CallbackInfo ci) {
-        this.cache = Maps.newConcurrentMap();
-    }
+    private Map<Block, List<StructureTemplate.StructureBlockInfo>> cache = Maps.newConcurrentMap();
 }
