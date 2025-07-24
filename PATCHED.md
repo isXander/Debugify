82c3547b24d6f56a4b823af0b04cbd577b7efdad
MicrocontrollersDev <66657148+MicrocontrollersDev@users.noreply.github.com>
2025-07-24 07:44:06 -0700

    fix MC-272431 (#459)
    * fix MC-272431
    
    * use expressions instead of arbitrary constant
    
    ---------
    
    Co-authored-by: Xander <xander@isxander.dev>

--- PATCHED.md
+++ PATCHED.md
@@ -110,6 +110,7 @@
 | Basic    | [MC-267125](https://bugs.mojang.com/browse/MC-267125) | Command suggestions for reloadable content are not affected by /reload                                                                         |
 | Basic    | [MC-268617](https://bugs.mojang.com/browse/MC-268617) | Structures can't be saved if the game directory is named in a certain way                                                                      |
 | Basic    | [MC-271899](https://bugs.mojang.com/browse/MC-271899) | StructureTemplate Palette's caches are not thread safe                                                                                         |
+| Basic    | [MC-272431](https://bugs.mojang.com/browse/MC-272431) | Ender Dragon incorrect vertical velocity causes erratic behavior                                                                               |
 | Basic    | [MC-298066](https://bugs.mojang.com/browse/MC-298066) | Directly entering a bed from a mount places the player in the wrong place                                                                      |
 
 ## Previously patched
---
+++ src/main/java/dev/isxander/debugify/mixins/basic/mc272431/EnderDragonMixin.java
@@ -0,0 +1,27 @@
+package dev.isxander.debugify.mixins.basic.mc272431;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import dev.isxander.debugify.fixes.BugFix;
+import dev.isxander.debugify.fixes.FixCategory;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@BugFix(id = "MC-272431", category = FixCategory.BASIC, env = BugFix.Env.SERVER, description = "Ender Dragon incorrect vertical velocity causes erratic behavior")
+@Mixin(EnderDragon.class)
+public class EnderDragonMixin {
+    @ModifyExpressionValue(method = "aiStep", at = @At(value = "CONSTANT", args = "doubleValue=0.01"))
+    private double fixTargetPath(double value) {
+        return 0.1;
+    }
+
+    @Definition(id = "setDeltaMovement", method = "Lnet/minecraft/world/entity/boss/enderdragon/EnderDragon;setDeltaMovement(Lnet/minecraft/world/phys/Vec3;)V")
+    @Definition(id = "add", method = "Lnet/minecraft/world/phys/Vec3;add(DDD)Lnet/minecraft/world/phys/Vec3;")
+    @Expression("?.setDeltaMovement(?.add(0.0, ? * @(0.01), 0.0))")
+    @ModifyExpressionValue(method = "aiStep", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private double fixVerticalVelocity(double value) {
+        return 0.1;
+    }
+}
