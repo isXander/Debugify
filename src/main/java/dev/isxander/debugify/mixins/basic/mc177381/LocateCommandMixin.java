5488173ac8b689d9145c786aec91c2b596d72084
isxander <xander@isxander.dev>
2025-09-30 20:15:00 +0100

    remove bugs that have been fixed in the last snapshot cycle

--- src/main/java/dev/isxander/debugify/mixins/basic/mc177381/LocateCommandMixin.java
+++ src/main/java/dev/isxander/debugify/mixins/basic/mc177381/LocateCommandMixin.java
@@ -1,4 +1,4 @@
-package dev.isxander.debugify.mixins.basic.mc17738;
+package dev.isxander.debugify.mixins.basic.mc177381;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -7,7 +7,7 @@ import dev.isxander.debugify.fixes.FixCategory;
 import net.minecraft.server.commands.LocateCommand;
 import org.spongepowered.asm.mixin.Mixin;
 
-@BugFix(id = "MC-177831", category = FixCategory.BASIC, env = BugFix.Env.SERVER, description = "Game does not count the distance properly if you locate a structure from more than 46340 blocks away")
+@BugFix(id = "MC-177381", category = FixCategory.BASIC, env = BugFix.Env.SERVER, description = "Game does not count the distance properly if you locate a structure from more than 46340 blocks away")
 @Mixin(LocateCommand.class)
 public class LocateCommandMixin {
     @WrapMethod(method = "dist")
