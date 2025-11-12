5e6cc70d6d53fd422a54c1432762cc4945b01832
isxander <xander@isxander.dev>
2025-11-12 17:50:14 +0000

    fix MC-158900 (thanks Microcontrollers #482)

---
+++ src/main/java/dev/isxander/debugify/mixins/basic/mc158900/PlayerListMixin.java
@@ -0,0 +1,22 @@
+package dev.isxander.debugify.mixins.basic.mc158900;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.isxander.debugify.fixes.BugFix;
+import dev.isxander.debugify.fixes.FixCategory;
+import net.minecraft.server.players.NameAndId;
+import net.minecraft.server.players.PlayerList;
+import net.minecraft.server.players.UserBanList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@BugFix(id = "MC-158900", category = FixCategory.BASIC, env = BugFix.Env.SERVER, description = "\"bad packet id 26\" disconnect after first-login after a temporary ban expires")
+@Mixin(PlayerList.class)
+public class PlayerListMixin {
+    @WrapOperation(method = "canPlayerLogin", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/UserBanList;isBanned(Lnet/minecraft/server/players/NameAndId;)Z"))
+    private boolean removeExpiredBeforeCheck(UserBanList instance, NameAndId user, Operation<Boolean> original) {
+        instance.get(user); // get will remove expired bans
+        return original.call(instance, user);
+    }
+
+}
