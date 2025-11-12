464ef2bc633c543e87fc0ebef3822dda2ed4c594
isxander <xander@isxander.dev>
2025-11-12 16:41:45 +0000

    fix MC-264979 (thanks Microcontrollers #477)

---
+++ src/main/java/dev/isxander/debugify/mixins/basic/mc264979/SettingsMixin.java
@@ -0,0 +1,24 @@
+package dev.isxander.debugify.mixins.basic.mc264979;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import dev.isxander.debugify.fixes.BugFix;
+import dev.isxander.debugify.fixes.FixCategory;
+import net.minecraft.server.dedicated.Settings;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+@BugFix(id = "MC-264979", category = FixCategory.BASIC, env = BugFix.Env.SERVER, description = "Fresh installations print NoSuchFileException when attempting to load server.properties")
+@Mixin(Settings.class)
+public class SettingsMixin {
+    @WrapMethod(method = "loadFromFile")
+    private static Properties dontLoadMissingFile(Path path, Operation<Properties> original) {
+        if (Files.notExists(path)) {
+            return new Properties();
+        }
+        return original.call(path);
+    }
+}
